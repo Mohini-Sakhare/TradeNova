52610080252d6461b89f3c78ac398853c986ade2
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-25 22:59:12 +0530

    add file

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -5,7 +5,11 @@ const OrdersSchema = new mongoose.Schema({
   qty: { type: Number, required: true, min: 1 },
   price: { type: Number, required: true, min: 0 },
   mode: { type: String, required: true }
-});
+},
+{
+  timestamps:true
+}
+);
 
 const OrdersModel = mongoose.model("order", OrdersSchema);
 
