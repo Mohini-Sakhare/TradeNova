121f2ac11ec56a2074d7508ecf5544d53148d7e4
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-25 23:01:34 +0530

    add home.jsx

---
+++ dashboard/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import DashBoard from "./DashBoard.jsx";
+import TopBar from "./TopBar.jsx";
+
+export default function Home() {
+  return (
+    <>
+      <TopBar />
+      <DashBoard />
+    </>
+  );
+};
