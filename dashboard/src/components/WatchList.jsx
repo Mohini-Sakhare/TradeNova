5b0a885da06634966b8c694bc7f3e5844ec8f872
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-25 23:04:08 +0530

    add watchlist file

---
+++ dashboard/src/components/WatchList.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useContext } from "react";
+import GeneralContext from "../context/GeneralContext";
+import { watchlist } from "../Data";
+import { DoughnutChart } from "../DataDiagrams/DoughnoutChart";
+import { FaChartBar, FaEllipsisH } from "react-icons/fa";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+const labels = watchlist.map((subArray) => subArray["name"]);
+
+const WatchList = () => {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Price",
+        data: watchlist.map((stock) => stock.price),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div className="watchlist-container">
+      <div className="search-container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
+          className="search"
+        />
+        <span className="counts">{watchlist.length} / 50</span>
+      </div>
+
+      <ul className="list">
+        {watchlist.map((stock, index) => (
+          <WatchListItem stock={stock} key={index} />
+        ))}
+      </ul>
+
+      <DoughnutChart data={data} />
+    </div>
+  );
+};
+
+export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setShowWatchlistActions] = useState(false);
+
+  return (
+    <li
+      className="watchlist-item"
+      onMouseEnter={() => setShowWatchlistActions(true)}
+      onMouseLeave={() => setShowWatchlistActions(false)}
+    >
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <MdKeyboardArrowDown className="arrow down" />
+          ) : (
+            <MdKeyboardArrowUp className="arrow up" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchlistActions && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  const { openBuyWindow, openSellWindow } = useContext(GeneralContext);
+
+  return (
+    <div className="actions">
+      <button className="btn buy" onClick={() => openBuyWindow(uid)}>
+        Buy
+      </button>
+      <button className="btn sell" onClick={() => openSellWindow(uid)}>
+        Sell
+      </button>
+      <button className="btn icon-btn">
+        <FaChartBar className="icon" />
+      </button>
+      <button className="btn icon-btn">
+        <FaEllipsisH className="icon" />
+      </button>
+    </div>
+  );
+};
