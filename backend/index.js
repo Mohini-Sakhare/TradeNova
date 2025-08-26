61345f55b0bdd734d2120f514cc93f620d827a4c
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-08-26 23:32:39 +0530

    add modified file

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ import cors from 'cors';
 import UserRouter from './Routes/UserRoutes.js';
 
 
-
 const PORT = process.env.PORT || 3001;
 const uri = process.env.MONGO_URL;
 
@@ -27,38 +26,6 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api/user", UserRouter);
 
-
-// app.get('/allHoldings', async(req, res)=>{
-//     let allHoldings = await HoldingsModel.find({});
-//     res.json(allHoldings);
-// });
-
-// app.get("/allPositions", async(req, res)=>{
-//     let allPositions = await PositionsModel.find({});
-//     res.json(allPositions);
-// });
-
-// app.post("/newOrder", async (req, res) => {
-//   try {
-//     const newOrder = new OrdersModel({
-//       name: req.body.name,
-//       qty: req.body.qty,
-//       price: req.body.price,
-//       mode: req.body.mode,
-//     });
-
-//     await newOrder.save();
-//     res.send("Order saved!");
-//   } catch (error) {
-//     console.error("Error saving order:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// app.get("/allOrders", async(req, res) =>{
-//   let allOrders = await OrdersModel.find({});
-//   res.json(allOrders);
-// })
 
 const start = async()=>{
   app.listen(PORT, ()=>{
