e7ad8968e8c75260bede0985851503d7d324d96d
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-09-01 23:49:59 +0530

    modify main.jsx

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -4,15 +4,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home";
 import { UserContext } from "./context/UserContext";
-// import Signup from "./Authentication/Signup";
 
 createRoot(document.getElementById('root')).render(
  
     <BrowserRouter>
     <UserContext>
        <Routes>
-        {/* <Route path="/*" element={<Signup/>} /> */}
-        <Route path="/*" element={<Home />} />
+        <Route path="/*" element={<Home/>}/>
       </Routes>
     </UserContext>
     </BrowserRouter>
