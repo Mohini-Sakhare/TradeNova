249a729bed293bb9deb7c2c647e1e26ccdf89de9
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-09-06 23:36:25 +0530

    Modify DashBoard.jsx file

--- dashboard/src/components/DashBoard.jsx
+++ dashboard/src/components/DashBoard.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Auth from "../Authentication/Auth";
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Funds from "./Funds";
 import Holdings from "../DataComponents/Holdings";
-
 import Orders from "../DataComponents/Orders";
 import Positions from "../DataComponents/Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
 import { GeneralContextProvider } from "../context/GeneralContext";
+import { UserDataContext } from "../context/UserContext";
+
 
 const Dashboard = () => {
+   const {userData}=useContext(UserDataContext);
   return (
     <div className="dashboard-container">
       <GeneralContextProvider>
@@ -18,12 +19,11 @@ const Dashboard = () => {
       </GeneralContextProvider>
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Summary />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/holdings" element={<Holdings />} />
-          <Route path="/positions" element={<Positions />} />
-          <Route path="/funds" element={<Funds />} />
-          <Route path="/auth" element={<Auth />} />
+          <Route path="/" element={<Summary/>} />
+          <Route path="orders" element={<Orders/>}/>
+          <Route path="holdings" element={<Holdings />}/>
+          <Route path="positions" element={<Positions/>}/>
+          <Route path="funds" element={<Funds />} />
         </Routes>
       </div>
     </div>
