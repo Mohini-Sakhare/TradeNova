90e0be779c63d1137175e90234e02c16f096403b
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-09-06 23:31:08 +0530

    modify main.jsx file

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import Home from "./components/Home";
 import { UserContext } from "./context/UserContext";
+import Signup from "./Authentication/Auth";
+import Home from "./components/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 
 createRoot(document.getElementById('root')).render(
  
     <BrowserRouter>
     <UserContext>
        <Routes>
-        <Route path="/*" element={<Home/>}/>
+        <Route path="/" element={<Signup/>}/>
+        <Route path="/home/*" element={
+          <ProtectedRoute>
+               <Home/>
+          </ProtectedRoute>
+          }/>
       </Routes>
     </UserContext>
     </BrowserRouter>
