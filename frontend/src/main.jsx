08bd36946028041958d3adb1bda69eb74925f941
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-09-09 23:54:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css'
 import HomePage from './landing-page/home/HomePage';
-import Signup from './landing-page/signup/Signup';
+// import Signup from './landing-page/signup/Signup';
 import About from "./landing-page/about/Aboutpage";
 import Products from './landing-page/products/ProductsPage';
 import Pricing from './landing-page/pricing/PricingPage';
